0bae0155fc751a4f559433b397cd48b8becbed30
Aditya Inapurapu <adityaii@gmail.com>
2019-03-11 23:39:17 -0700

    Edited 00_scratch.sh

--- 00_scratch.sh
+++ 00_scratch.sh
@@ -1,4 +1,4 @@
-docker rm -f liberty-app
+docker rm -f liberty-app localhost:5000/liberty-app
 docker rmi -f liberty-app maven-builder
 docker rm -f registry
 docker run -d -p 5000:5000 --name registry registry
